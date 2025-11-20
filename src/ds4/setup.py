f291eb3141d6b490ec217b758a46a9488183a62e
Tobias Thorgren <thorgrentobias@gmail.com>
2025-11-20 16:28:12 +0100

    2

--- src/ds4/setup.py
+++ src/ds4/setup.py
@@ -1,4 +1,3 @@
-import os
 from setuptools import find_packages, setup
 
 package_name = 'ds4'
@@ -11,8 +10,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
-    ],
+        ('share/' + package_name + '/launch', ['launch/ds4_launch.launch.py']),    
+        ],
+    
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='tobbe',
