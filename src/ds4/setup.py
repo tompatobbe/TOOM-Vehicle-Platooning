1307a70c70734fdad91d6d4caa1e562f3b174ba6
Tobias Thorgren <thorgrentobias@gmail.com>
2025-11-24 13:45:15 +0100

    setup update

--- src/ds4/setup.py
+++ src/ds4/setup.py
@@ -11,6 +11,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', ['launch/ds4_launch.launch.py']),    
+        ('share/' + package_name + '/launch', ['launch/leader.launch.py']),
         ],
     
     install_requires=['setuptools'],
