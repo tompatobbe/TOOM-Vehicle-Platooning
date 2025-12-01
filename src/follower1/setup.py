e764383f27747ae9a738f4c2512d76ed54a70059
Tobias Thorgren <thorgrentobias@gmail.com>
2025-12-01 13:49:34 +0100

    fix: setup

--- src/follower1/setup.py
+++ src/follower1/setup.py
@@ -25,7 +25,7 @@ setup(
     entry_points={
         'console_scripts': [
             'servo_driver = follower1.servo_driver:main',
-            'distance = follower1.distance:sonar_publisher',
+            'distance = follower1.distance:main',
         ],
     },
 )
