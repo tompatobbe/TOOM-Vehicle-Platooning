0f250a83ef0c89cbfbef003c556da0cb4baed54f
Tobias Thorgren <thorgrentobias@gmail.com>
2025-12-10 14:19:00 +0100

    follower2 launch file setup

---
+++ src/follower2/launch/follower2.launch.py
@@ -0,0 +1,67 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    
+    # --- NODE 1 CONFIGURATION ---
+    node1 = Node(
+        package='follower2',       # The name of the package where the node is defined
+        executable='distance', # The name of the executable (defined in setup.py or CMakeLists.txt)
+        name='distance',      # (Optional) Overwrite the node name
+        output='screen',                # Logs output to the console
+        parameters=[
+            {'param_name': 'value'}     # (Optional) Dictionary for parameters
+        ]
+    )
+
+    # --- NODE 2 CONFIGURATION ---
+    node2 = Node(
+        package='follower2',       # Can be the same package as Node 1
+        executable='controller',
+        name='controller',
+        output='screen',
+        # remappings=[                  # (Optional) Remap topics if necessary
+        #     ('/old_topic', '/new_topic')
+        # ]
+    )
+
+    node3 = Node(
+        package='follower2',       # Can be the same package as Node 1
+        executable='motor_driver',
+        name='motor_driver',
+        output='screen',
+        # remappings=[                  # (Optional) Remap topics if necessary
+        #     ('/old_topic', '/new_topic')
+        # ]
+    )
+
+    node4 = Node(
+        package='follower2',       # Can be the same package as Node 1
+        executable='pixy2_driver',
+        name='pixy2_driver',
+        output='screen',
+        # remappings=[                  # (Optional) Remap topics if necessary
+        #     ('/old_topic', '/new_topic')
+        # ]
+    )
+
+    node5 = Node(
+        package='follower2',       # Can be the same package as Node 1
+        executable='servo_driver',
+        name='servo_driver',
+        output='screen',
+        # remappings=[                  # (Optional) Remap topics if necessary
+        #     ('/old_topic', '/new_topic')
+        # ]
+    )
+
+    # --- RETURN LAUNCH DESCRIPTION ---
+    return LaunchDescription([
+        node1,
+        node2,
+        node3,
+        node4,
+        node5,
+    ])
