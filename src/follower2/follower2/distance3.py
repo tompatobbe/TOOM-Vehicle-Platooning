abda2efe3d65e7df2415120ef04b1b963dc2c2ed
Tobias Thorgren <thorgrentobias@gmail.com>
2025-12-11 14:39:05 +0100

    restored distance3 tests

--- src/follower2/follower2/distance3.py
+++ src/follower2/follower2/distance3.py
@@ -12,16 +12,16 @@ class MultiUltrasonicNode(Node):
         
         # --- Configuration ---
         # 1. Center Sensor (Existing)
-        self.gpio_trig_c = 17 #5
-        self.gpio_echo_c = 27 #6
+        self.gpio_trig_c = 5
+        self.gpio_echo_c = 6
         
         # 2. Left Sensor (NEW - UPDATE THESE PINS)
-        self.gpio_trig_l = 22 #17 
-        self.gpio_echo_l = 23 #27
+        self.gpio_trig_l = 17 
+        self.gpio_echo_l = 27
         
         # 3. Right Sensor (NEW - UPDATE THESE PINS)
-        self.gpio_trig_r = 5 #22
-        self.gpio_echo_r = 6 #23
+        self.gpio_trig_r = 22
+        self.gpio_echo_r = 23
 
         self.min_range = 0.02
         self.max_range = 4.0
