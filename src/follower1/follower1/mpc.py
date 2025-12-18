1879f6c4e79fcba0f7b97fe40c182e0e54ae11b9
Tobias Thorgren <thorgrentobias@gmail.com>
2025-12-18 14:04:48 +0100

    daeasd

--- src/follower1/follower1/mpc.py
+++ src/follower1/follower1/mpc.py
@@ -239,7 +239,7 @@ class PlatoonMPCNode(Node):
 
 
         if u_cmd > 0.1:            # Add friction deadband to start moving
-            compensated_cmd = np.sqrt(u_cmd) - 0.1
+            compensated_cmd = np.sqrt(u_cmd) - 0.05
 
         elif u_cmd > 0.01:
             # Add friction deadband to start moving
